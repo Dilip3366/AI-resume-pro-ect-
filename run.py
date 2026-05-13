dc82ef97c406d3c394b329c633b6ea075de970df
Dilip3366 <jogadilip.23.cse@anits.edu.in>
2026-05-13 12:23:47 +0530

    Clean run.py

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 from app import create_app
-from flask import send_from_directory
-import os
 
 app = create_app()
-
-@app.route('/')
-def index():
-    static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-    return send_from_directory(static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
