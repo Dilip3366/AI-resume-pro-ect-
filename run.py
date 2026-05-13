b6ac34b8b27cb6ebfc41279235f91261e7f701e8
Dilip3366 <jogadilip.23.cse@anits.edu.in>
2026-05-13 12:08:15 +0530

    Add root route

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from flask import send_from_directory
 
 app = create_app()
 
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
